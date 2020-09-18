13f8c18e8548b11c75b97a0fb812a361732556e6
Ricardo Ebbers <recl@cin.ufpe.br>
2020-09-17 22:01:34 -0300

    Created Eureka Server Registry

---
+++ registry/src/test/kotlin/br/ufpe/cin/recl/registry/RegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.ufpe.cin.recl.registry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
